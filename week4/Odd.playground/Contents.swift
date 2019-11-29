4103a50911bc1f2bec66f70a71ecef1ade79a055
Bacteriafish <you@example.com>
2019-11-29 11:13:34 +0800

    modify week4/Odd.playground

--- week4/Odd.playground/Contents.swift
+++ week4/Odd.playground/Contents.swift
@@ -1,6 +1,8 @@
 let isOdd = { (input: Int) -> Bool in
-  return input % 2 == 1
+  return input % 2 != 0
 }
 
 let sevenIsOdd = isOdd(7)
 let tenIsOdd = isOdd(10)
+let negativeNine = isOdd(-9)
+let negativeFour = isOdd(-4)
